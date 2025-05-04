0ea5edc57544efbd0efedcc4f0314bd520ebd017
Kaialogen <kaialogen@protonmail.com>
2025-05-04 17:29:27 +0100

    Feat: Update Contact component to include social media links and remove placeholder text

--- src/components/Layout/Contact.tsx
+++ src/components/Layout/Contact.tsx
@@ -1,7 +1,19 @@
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+
 const Contact = () => (
     <section className="p-6 rounded-2xl bg-zinc-800 shadow-md h-full">
       <h1 className="text-2xl font-bold">Contact</h1>
-      <p className="mt-2 text-zinc-400">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id similique necessitatibus voluptates enim, itaque obcaecati sint minima excepturi laudantium? Corporis nihil similique animi incidunt pariatur. Ipsa ipsum facilis minus molestias?</p>
+      <div className="mt-6 flex justify-center items-center">
+        <div className="flex items-center justify-between w-72">
+          <FaLinkedin className="w-12 h-12 text-zinc-400 mt-4" />
+            <a href="https://www.linkedin.com/in/kai-constantine-834952197/" target="_blank" rel="noopener noreferrer" className="text-zinc-300 hover:text-zinc-100">LinkedIn</a>
+          <FaGithub className="w-12 h-12 text-zinc-400 mt-4" />
+            <a href="https://github.com/Kaialogen" target="_blank" rel="noopener noreferrer" className="text-zinc-300 hover:text-zinc-100">GitHub</a>
+          <FaSquareXTwitter className="w-12 h-12 text-zinc-400 mt-4" />
+            <a href="https://twitter.com/Kaialogen" target="_blank" rel="noopener noreferrer" className="text-zinc-300 hover:text-zinc-100">X</a>
+        </div>
+      </div>
     </section>
   )
   
