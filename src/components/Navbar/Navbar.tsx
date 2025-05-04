b8770adb1f1f6e31525f5214b9fb01a13c49e206
Kaialogen <kaialogen@protonmail.com>
2025-05-04 12:39:53 +0100

    feat: Added Navbar componet

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar: React.FC = () => {
+    return (
+      <nav className="flex flex-col justify-between w-64 h-screen px-4 py-6 bg-zinc-900 text-white">
+        <div>
+        </div>
+        <div>
+        </div>
+      </nav>
+    )
+  }
+  
+  export default Navbar
