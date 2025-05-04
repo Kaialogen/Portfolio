c6629fdd434dd339db5c1d368e0c819dfc661f56
Kaialogen <kaialogen@protonmail.com>
2025-05-04 18:01:45 +0100

    Feat: Enhance Intro and Avatar components with profile image and location details

--- src/components/Layout/Intro.tsx
+++ src/components/Layout/Intro.tsx
@@ -2,7 +2,10 @@ const Intro = () => (
     <section className="p-6 rounded-2xl bg-zinc-800 shadow-md h-full">
       <h1 className="text-2xl font-bold">Introduction</h1>
       <p className="text-zinc-300 mb-4">
+        <br />
           <span className="block mb-1">Front-End Developer | Cybersecurity Enthusiast</span>
+          <span className="block mb-1">Based in the UK</span>
+          <br />
           I specialise in crafting secure, accessible, and performant web apps using JavaScript, React, and Tailwind CSS.
           My background in cybersecurity gives me a security-first mindset when building interfaces.
         </p>
--- src/components/Navbar/Avatar.tsx
+++ src/components/Navbar/Avatar.tsx
@@ -1,6 +1,10 @@
+import ProfileImage from './assets/Profile.PNG';
+
 const Avatar = () => (
     <div className="mb-6">
-      <div className="w-20 h-20 rounded-full bg-zinc-700 mx-auto" />
+      <div className="w-20 h-20 rounded-full bg-zinc-700 mx-auto overflow-hidden">
+        <img src={ProfileImage} alt="Kai Constantine" className="w-full h-full object-cover" />
+      </div>
       <p className="text-center mt-2 text-sm text-zinc-300">Kai Constantine</p>
     </div>
   )
