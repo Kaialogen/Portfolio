16d0cced582c7283409edd45f678d614337f7b96
Kaialogen <kaialogen@protonmail.com>
2025-05-04 17:53:54 +0100

    Feat: Update Experience component to include details of summer internship at Forvis Mazars LLP

--- src/components/Layout/Experience.tsx
+++ src/components/Layout/Experience.tsx
@@ -1,7 +1,11 @@
 const Experience = () => (
     <section className="p-6 rounded-2xl bg-zinc-800 shadow-md h-full">
       <h1 className="text-2xl font-bold">Experience</h1>
-      <p className="mt-2 text-zinc-400">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id similique necessitatibus voluptates enim, itaque obcaecati sint minima excepturi laudantium? Corporis nihil similique animi incidunt pariatur. Ipsa ipsum facilis minus molestias?</p>
+      <div className="mt-4">
+        <p className="mt-4 text-left font-bold">Summer Internship at Forvis Mazars LLP</p>
+        <p className="mt-4 text-zinc-400 text-left">June 2023 - August 2023</p>
+        <p className="mt-4 text-zinc-400 text-left">Internship in cyber attack and defence. Focused on projects including c2 infrastructure, malware EDR evasion techniques and IoT penetration testing.</p>
+      </div>
     </section>
   )
   
