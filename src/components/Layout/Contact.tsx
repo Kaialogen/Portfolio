7ded49740a0447ff8ded2a30110c436558470bc5
Kaialogen <kaialogen@protonmail.com>
2025-05-04 17:39:06 +0100

    Feat: Improve layout of Contact component by restructuring social media links

--- src/components/Layout/Contact.tsx
+++ src/components/Layout/Contact.tsx
@@ -6,15 +6,21 @@ const Contact = () => (
       <h1 className="text-2xl font-bold">Contact</h1>
       <div className="mt-6 flex justify-center items-center">
         <div className="flex items-center justify-between w-72">
-          <FaLinkedin className="w-12 h-12 text-zinc-400 mt-4" />
-            <a href="https://www.linkedin.com/in/kai-constantine-834952197/" target="_blank" rel="noopener noreferrer" className="text-zinc-300 hover:text-zinc-100">LinkedIn</a>
-          <FaGithub className="w-12 h-12 text-zinc-400 mt-4" />
-            <a href="https://github.com/Kaialogen" target="_blank" rel="noopener noreferrer" className="text-zinc-300 hover:text-zinc-100">GitHub</a>
-          <FaSquareXTwitter className="w-12 h-12 text-zinc-400 mt-4" />
-            <a href="https://twitter.com/Kaialogen" target="_blank" rel="noopener noreferrer" className="text-zinc-300 hover:text-zinc-100">X</a>
+              <a href="https://www.linkedin.com/in/kai-constantine-834952197/" target="_blank" rel="noopener noreferrer" className="flex flex-col items-center text-zinc-300 hover:text-zinc-100">
+              <FaLinkedin className="w-12 h-12 text-zinc-400 mt-4" />
+              LinkedIn
+              </a>
+              <a href="https://github.com/Kaialogen" target="_blank" rel="noopener noreferrer" className="flex flex-col items-center text-zinc-300 hover:text-zinc-100">
+              <FaGithub className="w-12 h-12 text-zinc-400 mt-4" />
+              GitHub
+              </a>
+              <a href="https://twitter.com/Kaialogen" target="_blank" rel="noopener noreferrer" className="flex flex-col items-center text-zinc-300 hover:text-zinc-100">
+              <FaSquareXTwitter className="w-12 h-12 text-zinc-400 mt-4" />
+              X
+              </a>
         </div>
       </div>
     </section>
   )
   
-  export default Contact
+export default Contact
