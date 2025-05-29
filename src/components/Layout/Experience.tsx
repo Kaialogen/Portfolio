6ab1343b493f1249c3df7e30ada31e1fd5320d5f
Kaialogen <kaialogen@protonmail.com>
2025-05-29 22:40:01 +0100

    feat: Update Experience and Intro components; correct titles and enhance descriptions

--- src/components/Layout/Experience.tsx
+++ src/components/Layout/Experience.tsx
@@ -1,12 +1,21 @@
 const Experience = () => (
-    <section className="p-6 rounded-2xl bg-zinc-800 shadow-md h-full">
-      <h1 className="text-2xl font-bold">Experience</h1>
-      <div className="mt-4">
-        <p className="mt-4 text-left font-bold">Summer Internship at Forvis Mazars LLP</p>
-        <p className="mt-4 text-zinc-400 text-left">July 2023 - August 2023</p>
-        <p className="mt-4 text-zinc-400 text-left">Internship in cyber attack and defence. Focused on projects including c2 infrastructure, malware EDR evasion techniques and IoT penetration testing.</p>
-      </div>
-    </section>
-  )
-  
-  export default Experience
+  <section className='p-6 rounded-2xl bg-zinc-800 shadow-md h-full'>
+    <h1 className='text-2xl font-bold'>Experience</h1>
+    <div className='mt-4'>
+      <p className='mt-4 text-left font-bold'>Summer Internship at Forvis Mazars LLP</p>
+      <p className='mt-4 text-zinc-400 text-left'>July 2023 - August 2023</p>
+      <p className='mt-4 text-zinc-400 text-left'>
+        Internship in cyber attack and defence. Focused on projects including c2 infrastructure, malware EDR evasion
+        techniques and IoT penetration testing.
+      </p>
+    </div>
+    <br />
+    <div className='mt-4'>
+      <p className='mt-4 text-left font-bold'>Cyber Security Graduate</p>
+      <p className='mt-4 text-zinc-400 text-left'>October 2021 - July 2024</p>
+      <p className='mt-4 text-zinc-400 text-left'>Graduated in Cyber Security from the University of Warwick.</p>
+    </div>
+  </section>
+);
+
+export default Experience;
--- src/components/Layout/Intro.tsx
+++ src/components/Layout/Intro.tsx
@@ -3,10 +3,10 @@ const Intro = () => (
     <h1 className='text-2xl font-bold'>Introduction</h1>
     <p className='text-zinc-400 mb-4'>
       <br />
-      <span className='block mb-1 text-zinc-300'>Front-End Developer | Cybersecurity Enthusiast</span>
+      <span className='block mb-1 text-zinc-300'>Full Stack Developer | Cybersecurity Enthusiast</span>
       <span className='block mb-1 text-zinc-300'>Based in the UK</span>
       <br />
-      Hey, I'm Kai, a dedicated Cyber Security undergrad and full-stack developer who thrives on building and shipping
+      Hey, I'm Kai, a dedicated Cyber Security graduate and full-stack developer who thrives on building and shipping
       solutions that truly matter. I work primarily with React.js, JavaScript, Node.js, and TypeScript to craft robust
       applications on both the frontend and backend.
       <br />
