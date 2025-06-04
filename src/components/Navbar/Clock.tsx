d828db5214c94cf148ebfe860575b03c238cb348
Kaialogen <kaialogen@protonmail.com>
2025-06-04 12:15:11 +0100

    feat: Update Clock component to display UK local time with improved formatting

--- src/components/Navbar/Clock.tsx
+++ src/components/Navbar/Clock.tsx
@@ -1,18 +1,23 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 
 const Clock = () => {
-  const [time, setTime] = useState(new Date())
+  const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    const interval = setInterval(() => setTime(new Date()), 1000)
-    return () => clearInterval(interval)
-  }, [])
+    const interval = setInterval(() => setTime(new Date()), 1000);
+    return () => clearInterval(interval);
+  }, []);
 
-  return (
-    <div className="text-center text-xs text-zinc-400 mt-4">
-      {time.toLocaleTimeString()}
-    </div>
-  )
-}
+  // Convert local time to Europe/London string
+  const gmtTime = time.toLocaleTimeString('en-GB', {
+    timeZone: 'Europe/London',
+    hour12: true, // optional: use 24-hour format
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  });
 
-export default Clock
+  return <div className='text-center text-xs text-zinc-400 mt-4'>{gmtTime} UK Local Time</div>;
+};
+
+export default Clock;
