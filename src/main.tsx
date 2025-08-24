21ee8893ae241d2e787a6a523e303ed5baf7a3ad
Kaialogen <kaialogen@protonmail.com>
2025-08-24 14:47:54 +0100

    feat: Remove Portfolio components and update routing in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
-import PortfolioPage from './components/Portfolio/PortfolioPage.tsx';
 import Blog from './pages/Blog.tsx';
 import './Styles.css';
 
@@ -11,8 +10,8 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App />} />
-        <Route path='/portfolio' element={<PortfolioPage />} />
         <Route path='/blog' element={<Blog />} />
+        <Route path='*' element={<App />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
